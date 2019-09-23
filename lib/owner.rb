2f6ff1ec2f956cf2468c1a04c7b9032ee7797eea
Doug Kintop <dkintop@hotmail.com>
2019-09-23 19:35:45 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,40 @@
+require 'pry'
 class Owner
-  # code goes here
+  attr_accessor :all, :cats, :dogs
+  attr_reader :species, :name
+  @@owners = []
+  def initialize(name)
+    @name = name 
+    @species = "human"
+    @@owners << self
+    @cats = []
+    @dogs = []
+  end
+  
+  def say_species
+    "I am a human."
+  end 
+  
+  def self.all 
+    @@owners
+  end
+  
+  def self.count
+    @@owners.size
+  end 
+  
+  def self.reset_all
+    @@owners.clear
+  end 
+  
+  def cats 
+    @cats
+  end 
+  
+  def dogs 
+    @dog
+  end
+  
+ 
+  
 end
